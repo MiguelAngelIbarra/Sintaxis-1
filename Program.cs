694b9a253a3562150f691af992d3f3c6950f1fe1
MiguelAngelIbarra <89610156+MiguelAngelIbarra@users.noreply.github.com>
2021-11-26 15:57:00 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Sintaxis_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Lenguaje L = new Lenguaje();
+           
+            try
+            {
+                L.Programa();
+            }
+            catch (Exception)
+            {
+               Console.WriteLine("Fin de Compilacion. Verifique el codigo");
+            }
+            
+           /* while(!L.FinAchivo())
+            {
+                L.NextToken();
+                
+            }*/
+
+        }
+    }
+}
